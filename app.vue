85f94dd3d69d780f77e3651a226e66401d56b488
jordanfurr <ejfurr@umich.edu>
2024-06-24 11:00:27 -0400

    initial

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ph3 pv1">
+    <div class="text-center">
+      <p id="logo">coat text</p>
+    </div>
+    <div class="mt3 w-90">
+      <div id="info" class="pa3 space-between">
+        <div id="info-section">
+          <p class="mb4">Coat Text</p>
+          <p>Coat Text is the Web Design & Development practice of Jordan Furr. Coat Text is based in Brooklyn, NY.</p>
+          <p>jordan@jordanfurr.com</p>
+        </div>
+        <div class="">
+          <p class="mb4">FEATURED WORK</p>
+          <div>
+            <p>SAMEWAVE7</p>
+            <p>damesCity</p>
+            <p>The Upside of Uncertainty</p>
+            <p>Harmon Psychotherapy & Consulting</p>
+            <p>Center for Expanding Compassion</p>
+            <p>Uncertainty Science Initiative</p>
+            <p>Made with Harmony</p>
+            <p>Star Laces LLC</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

00ec816de6bb3c21b4f592391c720123f033c4e4
jordanfurr <ejfurr@umich.edu>
2024-07-07 16:18:18 -0400

    july 7

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ph3 pv1">
     <div class="text-center">
-      <p id="logo">coat text</p>
+      <p id="logo">bad taste</p>
     </div>
     <div class="mt3 w-90">
       <div id="info" class="pa3 space-between">

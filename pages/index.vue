cf8a7e850e1c80b59da082cb752a2d36ea038f64
jordanfurr <ejfurr@umich.edu>
2024-07-07 20:33:07 -0400

    bad taste basics

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col w-100">
+        <div class="">
+            <p id="logo">bad taste</p>
+        </div>
+        <div class="info-section w-50 borderbox">
+            
+            <p>bad taste is the web design</p>
+            <p>& development practice of</p>
+            <p>Jordan Furr. bad taste is</p>
+            <p>based in Brooklyn, NY.</p>
+            <br>
+            <p>jordan@jordanfurr.com</p>
+        </div>
+        <div class="featured">
+            <p class="subtitle">FEATURED WORK</p>
+            <div class="site-list">
+                <p>SAMEWAVE7</p>
+                <p>damesCity</p>
+                <p>The Upside of Uncertainty</p>
+                <p>Harmon Psychotherapy & Consulting</p>
+                <p>Center for Expanding Compassion</p>
+                <p>Uncertainty Science Initiative</p>
+                <p>Made with Harmony</p>
+                <p>Star Laces LLC</p>
+            </div>
+        </div>
+    </div>
+</template>
